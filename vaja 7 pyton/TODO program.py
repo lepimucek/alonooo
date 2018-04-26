cb511ca476e9c1c1c67ffd275e81ec6fd4ced499
legenda <lepi.mucek@gmail.com>
2018-04-26 17:23:52 +0200

    TODO

---
+++ vaja 7 pyton/TODO program.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __builtin__ import raw_input
+
+from pip._internal.req.req_file import break_args_options
+
+print 'Welcome to our TODO'
+todo_list = []
+
+while True:
+    task= raw_input('Vnesi task: ')
+    print 'Your task is %s' % task
+    todo_list.append(task)
+
+    new = raw_input('Dodaj še enega? (YES/NO')
+    if new != 'YES':
+        breakk
+print 'All tasks: {0}'.format(todo_list)
+print 'END'
